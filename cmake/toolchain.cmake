d9bf561fa76bc9964b672a386b6e1f52725c6747
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-11-23 14:21:51 +0100

    Dump
    Signed-off-by: Sebastian Boe <sebastian.boe@nordicsemi.no>

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -8,12 +8,15 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
-# Configure the toolchain based on what SDK/toolchain is in use.
-if(ZEPHYR_GCC_VARIANT STREQUAL "host" OR CONFIG_ARCH_POSIX)
-	set(COMPILER host-gcc)
+if(CONFIG_ARCH_POSIX)
+  set(TOOLCHAIN_VARIANT host-gcc)
 else()
-include($ENV{ZEPHYR_BASE}/cmake/toolchain-${ZEPHYR_GCC_VARIANT}.cmake)
+  # Default
+  set(TOOLCHAIN_VARIANT ${ZEPHYR_GCC_VARIANT})
 endif()
+
+# Configure the toolchain based on what SDK/toolchain is in use.
+include($ENV{ZEPHYR_BASE}/cmake/toolchain-${TOOLCHAIN_VARIANT}.cmake)
 
 # Configure the toolchain based on what toolchain technology is used
 # (gcc clang etc.)
