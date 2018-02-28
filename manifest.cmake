d80221c402f10777117512c795df39c8332a352d
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2018-02-28 14:52:36 +0100

    First revision of manifest
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

---
+++ manifest.cmake
@@ -0,0 +1,33 @@
+# This is a manifest file akin to Repo's manifest, or a PKGBUILD's
+# dependency list. It is a CMakeList.txt-formatted file that defines
+# in a central place where external dependencies are taken from.
+
+# Currently it is supported to retrieve dependencies from remote git
+# repositories and local paths.
+
+# Header guard
+if(ZEPHYR_MANIFEST_CMAKE_GUARD)
+  return()
+else()
+  set(ZEPHYR_MANIFEST_CMAKE_GUARD 1)
+endif()
+
+if(USE_LOCAL_OPENTHREAD)
+  set(manifest_openthread_type_local 1)
+  set(manifest_openthread_type_local_path ${ZEPHYR_BASE}/../openthread)
+else()
+  # TODO: Point to a Zephyr fork
+  # Nov. 7
+  set(manifest_openthread_type_download 1)
+  set(manifest_openthread_type_download_repo https://github.com/openthread/openthread.git)
+  set(manifest_openthread_type_download_tag  a89eb887488dcbab7f5e9237e2bbcaad38140690)
+endif()
+
+if(USE_NRFX_FROM_ZEPHYR_REPO)
+  set(manifest_nrfx_type_local 1)
+  set(manifest_nrfx_type_local_path ${ZEPHYR_BASE}/ext/hal/nordic/nrfx)
+else()
+  set(manifest_nrfx_type_download 1)
+  set(manifest_nrfx_type_download_repo https://github.com/NordicSemiconductor/nrfx)
+  set(manifest_nrfx_type_download_tag  v0.8.0)
+endif()
